c20b7ed4d7038386e8102d40a357c0531b4e8543
Azmoria <65363489+Azmoria@users.noreply.github.com>
2022-12-04 00:29:26 -0500

    pass without a trace

--- Main.js
+++ Main.js
@@ -1,23 +1,34 @@
-window.temporaryEffects = [];
-window.temporaryEffects['Mage Armor'] = 
-	{
+window.temporaryEffects = {};
+window.temporaryEffects['Mage Armor'] = {
 		name: 'Mage Armor',
-		baseArmorMod: '3'	
+		magicArmorMod: '3'
 	}
+window.temporaryEffects['Pass without a Trace'] = { 
+		name: 'Pass without a Trace',
+		skillMod: {
+			Stealth: '10'
+		},
+}
+
 
 
 
 function setArmorMagicBonus(feature){
 	$(`.ct-character-sheet`).click();
 	$(".ddbc-armor-class-box").click();
-	$(`.ct-armor-manage-pane .ddbc-collapsible__header`).click();	
-	let reactProps = getReactProps(`.ct-value-editor__property--2 .ct-value-editor__property-value input`);
-	let currentValue = parseInt($(`.ct-value-editor__property--2 .ct-value-editor__property-value input`)[0][reactProps].value);
-	$(`.ct-value-editor__property--2 .ct-value-editor__property-value input`)[0][reactProps].value = currentValue + parseInt(feature.baseArmorMod);
-	$(`.ct-value-editor__property--2 .ct-value-editor__property-value input`)[0][reactProps].onBlur({target: $(`.ct-value-editor__property--2 .ct-value-editor__property-value input`)[0][reactProps]})
-	reactProps = getReactProps(`.ct-value-editor__property--2 .ct-value-editor__property-source input`);
-	$(`.ct-value-editor__property--2 .ct-value-editor__property-source input`)[0][reactProps].value += feature.name;
-	$(`.ct-value-editor__property--2 .ct-value-editor__property-source input`)[0][reactProps].onBlur({target: $(`.ct-value-editor__property--2 .ct-value-editor__property-source input`)[0][reactProps]})
+	$(`.ct-skill-pane .ddbc-collapsible__header`).click();	
+	let reactProps = getReactProps(`.ct-value-editor__property--24 .ct-value-editor__property-value input`);
+	let currentValue = parseInt($(`.ct-value-editor__property--24 .ct-value-editor__property-value input`)[0][reactProps].value);
+	
+	currentValue = isNaN(currentValue) ?  0 : currentValue;
+	
+	$(`.ct-value-editor__property--24 .ct-value-editor__property-value input`)[0][reactProps].value = currentValue + parseInt(feature.magicArmorMod);
+	$(`.ct-value-editor__property--24 .ct-value-editor__property-value input`)[0][reactProps].onBlur({target: $(`.ct-value-editor__property--24 .ct-value-editor__property-value input`)[0][reactProps]})
+		
+	reactProps = getReactProps(`.ct-value-editor__property--24 .ct-value-editor__property-source input`);
+	
+	$(`.ct-value-editor__property--24 .ct-value-editor__property-source input`)[0][reactProps].value += feature.name;
+	$(`.ct-value-editor__property--24 .ct-value-editor__property-source input`)[0][reactProps].onBlur({target: $(`.ct-value-editor__property--24 .ct-value-editor__property-source input`)[0][reactProps]})
 	$(`.ct-character-sheet`).click();
 }
 
@@ -25,15 +36,68 @@ function removeArmorMagicBonus(feature){
 	$(`.ct-character-sheet`).click();
 	$(".ddbc-armor-class-box").click();
 	$(`.ct-armor-manage-pane .ddbc-collapsible__header`).click();	
+
 	let reactProps = getReactProps(`.ct-value-editor__property--2 .ct-value-editor__property-value input`);
 	let currentValue = parseInt($(`.ct-value-editor__property--2 .ct-value-editor__property-value input`)[0][reactProps].value);
-	$(`.ct-value-editor__property--2 .ct-value-editor__property-value input`)[0][reactProps].value = currentValue - parseInt(feature.baseArmorMod);
-	$(`.ct-value-editor__property--2 .ct-value-editor__property-value input`)[0][reactProps].onBlur({target: $(`.ct-value-editor__property--2 .ct-value-editor__property-value input`)[0][reactProps]})
+	currentValue = isNaN(currentValue) ?  0 : currentValue;
+
+	$(`.ct-value-editor__property--24 .ct-value-editor__property-value input`)[0][reactProps].value = currentValue - parseInt(feature.magicArmorMod);
+	$(`.ct-value-editor__property--24 .ct-value-editor__property-value input`)[0][reactProps].onBlur({target: $(`.ct-value-editor__property--2 .ct-value-editor__property-value input`)[0][reactProps]})
+	
 	reactProps = getReactProps(`.ct-value-editor__property--2 .ct-value-editor__property-source input`);
-	$(`.ct-value-editor__property--2 .ct-value-editor__property-source input`)[0][reactProps].value = $(`.ct-value-editor__property--2 .ct-value-editor__property-source input`)[0].value.replace(feature.name, "");
-	$(`.ct-value-editor__property--2 .ct-value-editor__property-source input`)[0][reactProps].onBlur({target: $(`.ct-value-editor__property--2 .ct-value-editor__property-source input`)[0][reactProps]})
+
+	$(`.ct-value-editor__property--24 .ct-value-editor__property-source input`)[0][reactProps].value = $(`.ct-value-editor__property--2 .ct-value-editor__property-source input`)[0].value.replace(feature.name, "");
+	$(`.ct-value-editor__property--24 .ct-value-editor__property-source input`)[0][reactProps].onBlur({target: $(`.ct-value-editor__property--2 .ct-value-editor__property-source input`)[0][reactProps]})
 	$(`.ct-character-sheet`).click();
 }
+
+function setSkillBonus(feature){
+	$(`.ct-character-sheet`).click();
+	for(skill in feature.skillMod){
+		$(`.ct-skills__col--skill:contains(${skill})`).click();
+		$(`.ct-skill-pane .ddbc-collapsible__header`).click();	
+		
+		let reactProps = getReactProps(`.ct-value-editor__property--24 .ct-value-editor__property-value input`);
+		let currentValue = parseInt($(`.ct-value-editor__property--24 .ct-value-editor__property-value input`)[0][reactProps].value);
+		currentValue = isNaN(currentValue) ?  0 : currentValue;
+		
+		$(`.ct-value-editor__property--24 .ct-value-editor__property-value input`)[0][reactProps].value = currentValue + parseInt(feature.skillMod[skill]);
+		$(`.ct-value-editor__property--24 .ct-value-editor__property-value input`)[0][reactProps].onBlur({target: $(`.ct-value-editor__property--24 .ct-value-editor__property-value input`)[0][reactProps]})
+		
+		reactProps = getReactProps(`.ct-value-editor__property--24 .ct-value-editor__property-source input`);
+		
+		$(`.ct-value-editor__property--24 .ct-value-editor__property-source input`)[0][reactProps].value += feature.name;
+		$(`.ct-value-editor__property--24 .ct-value-editor__property-source input`)[0][reactProps].onBlur({target: $(`.ct-value-editor__property--24 .ct-value-editor__property-source input`)[0][reactProps]})
+		$(`.ct-character-sheet`).click();
+	}
+}
+
+function removeSkillBonus(feature){
+	$(`.ct-character-sheet`).click();
+	for(skill in feature.skillMod){
+		$(`.ct-skills__col--skill:contains(${skill})`).click();
+		$(`.ct-skill-pane .ddbc-collapsible__header`).click();	
+		
+		let reactProps = getReactProps(`.ct-value-editor__property--24 .ct-value-editor__property-value input`);
+		let currentValue = parseInt($(`.ct-value-editor__property--24 .ct-value-editor__property-value input`)[0][reactProps].value);
+		
+		currentValue = isNaN(currentValue) ?  0 : currentValue;
+		
+		$(`.ct-value-editor__property--24 .ct-value-editor__property-value input`)[0][reactProps].value = currentValue - parseInt(feature.skillMod[skill]);
+		$(`.ct-value-editor__property--24 .ct-value-editor__property-value input`)[0][reactProps].onBlur({target: $(`.ct-value-editor__property--24 .ct-value-editor__property-value input`)[0][reactProps]})
+		
+		reactProps = getReactProps(`.ct-value-editor__property--24 .ct-value-editor__property-source input`);
+		
+		$(`.ct-value-editor__property--24 .ct-value-editor__property-source input`)[0][reactProps].value = $(`.ct-value-editor__property--24 .ct-value-editor__property-source input`)[0].value.replace(feature.name, "");
+		$(`.ct-value-editor__property--24 .ct-value-editor__property-source input`)[0][reactProps].onBlur({target: $(`.ct-value-editor__property--24 .ct-value-editor__property-source input`)[0][reactProps]})
+		$(`.ct-character-sheet`).click();
+	}
+
+	
+}
+
+
+
 function getReactProps(selector){
 	for(i in $(selector)[0])
 	{
@@ -42,6 +106,7 @@ function getReactProps(selector){
 	    }
 	}
 }
+
 function buildStatus(){
 	let statusButton = "<div class='ct-primary-box__tab--extras ddbc-tab-list__nav-item statusEffects'>Status Effects</div>";
 	$(".ct-character-header-desktop").append(statusButton);
@@ -52,28 +117,38 @@ function buildStatus(){
 			$(".ct-sidebar__pane-content").empty();
 			buildStatusButtons();
 		}, 100)
-	
 	});   	
 }
+
 function buildStatusButtons(){
 	for(i in window.temporaryEffects){
 		let cleanedFeatureName = i.replace(/[^A-Z0-9]/ig, "");
-		let button = $(`<button id='button_${cleanedFeatureName}'>${i}</button>`);
+		let button = $(`<button id='button_${cleanedFeatureName}' data-name='${i}'>${i}</button>`);
 		$(".ct-sidebar__pane-content").append(button);		
-		if(i == 'Mage Armor'){
-			$(button).off().on('click', function(){
-				if(window.temporaryEffects['Mage Armor'].applied){
-					removeArmorMagicBonus(window.temporaryEffects['Mage Armor']);
-					delete window.temporaryEffects['Mage Armor'].applied;
-				}
-				else{
-					setArmorMagicBonus(window.temporaryEffects['Mage Armor']);
-					window.temporaryEffects['Mage Armor'].applied = true;
-				}
-				
-				console.log("CLICK!!!!!");
-			})
-		}
+			
+		$(button).off().on('click', function(){
+			let feature = $(this).attr("data-name");
+			
+			//MAGIC ARMOR BUTTONS
+			if(window.temporaryEffects[feature].applied && window.temporaryEffects[feature]['magicArmorMod'] != undefined){
+				removeArmorMagicBonus(window.temporaryEffects[feature]);
+				delete window.temporaryEffects[feature].applied;
+			}
+			else if(window.temporaryEffects[feature]['magicArmorMod'] != undefined){
+				setArmorMagicBonus(window.temporaryEffects[feature]);
+				window.temporaryEffects[feature].applied = true;
+			}
+
+			if(window.temporaryEffects[feature].applied && window.temporaryEffects[feature]['skillMod'] != undefined){
+				removeSkillBonus(window.temporaryEffects[feature]);
+				delete window.temporaryEffects[feature].applied;
+			}
+			else if(window.temporaryEffects[feature]['skillMod'] != undefined){
+				setSkillBonus(window.temporaryEffects[feature]);
+				window.temporaryEffects[feature].applied = true;
+			}
+		});
+
 	}
 }
 
